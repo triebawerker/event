856bf88efd4a6572db95c22612c7c153a65f982e
micha trieba <micha.trieba@mayflower.de>
2014-05-11 20:27:59 +0200

    added PspService

--- spec/BrokerSpec.php
+++ spec/BrokerSpec.php
@@ -16,4 +16,12 @@ class BrokerSpec extends ObjectBehavior
     {
         $this->shouldHaveType('Broker');
     }
+
+    public function it_should_have_a_PspService() {
+        $this->getPspService()->shouldHaveType('PspService');
+    }
+
+    public function it_should_get_approval_for_a_new_reservation() {
+
+    }
 }
