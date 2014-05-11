db7bb05731cbd109cb07d42b83b81765be8a514f
micha trieba <micha.trieba@mayflower.de>
2014-05-11 20:12:53 +0200

    introcuced ReservationService, which creates a Reservation

--- spec/ReservationServiceSpec.php
+++ spec/ReservationServiceSpec.php
@@ -6,11 +6,24 @@ use PhpSpec\ObjectBehavior;
 
 class ReservationServiceSpec extends ObjectBehavior
 {
+    function let($eventDispatcher)
+    {
+        $eventDispatcher->beADoubleOf('EventDispatcher');
+        $this->beConstructedWith($eventDispatcher);
+    }
+
     function it_is_initializable() {
         $this->shouldHaveType('ReservationService');
     }
 
     function it_should_create_a_reservation() {
-        $this->createReservation()->shouldHaveType('Reservation');
+        $this->createReservation();
+        $this->getReservation()->shouldHaveType('Reservation');
+    }
+
+    function it_should_emit_an_ReservationCreatedEvent($eventDispatcher) {
+        $this->createReservation();
+
+        $eventDispatcher->emit(new \ReservationCreatedEvent())->shouldBeCalled();
     }
 }
--- src/ReservationService.php
+++ src/ReservationService.php
@@ -3,8 +3,40 @@
 class ReservationService
 {
 
+    /** @var  Reservation $reservation */
+    private $reservation;
+
+    /** @var  EventDispatcher $eventDispatcher */
+    private $eventDispatcher;
+
+    public function __construct(EventDispatcher $eventDispatcher) {
+        $this->eventDispatcher = $eventDispatcher;
+    }
+
+    /**
+     * @return $this
+     */
     public function createReservation()
     {
-        return new Reservation(new EventDispatcher());
+        $this->reservation = new Reservation();
+        $this->getEventDispatcher()->emit(new ReservationCreatedEvent());
+
+        return $this;
+    }
+
+    /**
+     * @return Reservation
+     */
+    public function getReservation()
+    {
+        return $this->reservation;
+    }
+
+    /**
+     * @return EventDispatcher
+     */
+    public function getEventDispatcher()
+    {
+        return $this->eventDispatcher;
     }
 }
