12c9d1c1c14ad374b5c6c172979dc4ef198960f9
micha trieba <micha.trieba@mayflower.de>
2014-05-18 21:28:43 +0200

    added reservation status test Customer

---
+++ features/bootstrap/ReservationContext.php
@@ -0,0 +1,63 @@
+<?php
+
+use Behat\Behat\Context\BehatContext;
+use Behat\Behat\Exception\PendingException;
+
+class ReservationContext extends BehatContext
+{
+    /** @var  Customer $customer */
+    private $customer;
+
+    /** @var  Reservation $reservation */
+    private $reservation;
+
+    /**
+     * @Given /^there is a Customer$/
+     */
+    public function thereIsACustomer()
+    {
+        $this->customer = new Customer();
+    }
+
+    /**
+     * @When /^I create a reservation$/
+     */
+    public function iCreateAReservation()
+    {
+        $this->reservation = new Reservation();
+    }
+
+    /**
+     * @Then /^the approval status should be \"([^\"]*)\"$/
+     */
+    public function theApprovalStatusShouldBe($arg1)
+    {
+        assertEquals($this->reservation->getStatus(), $arg1);
+    }
+
+    /**
+     * @Given /^there is a Rerservation$/
+     */
+    public function thereIsARerservation()
+    {
+        throw new PendingException();
+    }
+
+    /**
+     * @When /^my reservation has the status "([^"]*)"$/
+     */
+    public function myReservationHasTheStatus($arg1)
+    {
+        throw new PendingException();
+    }
+
+    /**
+     * @Then /^the PSP should be called and return "([^"]*)"$/
+     */
+    public function thePspShouldBeCalledAndReturn($arg1)
+    {
+        throw new PendingException();
+    }
+
+
+}
